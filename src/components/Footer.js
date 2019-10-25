119e12f05c07fd747880a7d9b9d5854c2e609f2f
Mehmet Tahca <mtahca@Mehmets-iMac.local>
2019-10-25 17:14:38 +0300

    kat planları

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
 
 export default class Footer extends Component {
-    render() {
-        return (
-            <div className="container copyright-section">
-			<section className="eight columns">
-				Kıyanç İnşaat © 2014<br></br>Adres: Orhaniye Mah.Hürriyet Cad.Kahyaoğlu Apt Kat :4 No:15 Muğla<br></br>Tel: 0 252 214 04 48 - 0 252 214 44 08</section>
-			<section className="eight columns">
-				<ul className="footer-nav">
-					<li>
-                    <p>Teknofikir</p>
-                    </li>	
-				</ul>
-			</section>
-		</div>
-        )
-    }
+	render() {
+		return (
+			<div className="container copyright-section">
+				<section className="eight columns">
+					Kıyanç İnşaat © 2014<br></br>Adres: Orhaniye Mah.Hürriyet Cad.Kahyaoğlu Apt Kat :4 No:15 Muğla<br></br>Tel: 0 252 214 04 48 - 0 252 214 44 08</section>
+				<section className="eight columns">
+				</section>
+				<section class="eight columns">
+					<ul class="footer-nav">
+						<li>
+							<a href="http://www.teknofikir.com"
+							><img src="images/teknofikir.png" /></a>
+						</li>
+					</ul>
+				</section>
+			</div>
+		)
+	}
 }
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react'
 import SwiftSlider from 'react-swift-slider'
 
 const data = [
-	{ 'id': '1', 'src': 'images/sequence-slider/slider-bg-1.jpg' },
-	{ 'id': '2', 'src': 'images/sequence-slider/slider-bg-2.jpg' },
-	{ 'id': '3', 'src': 'images/sequence-slider/slider-bg-3.jpg' },
+	{ 'id': '1', 'src': 'images/slider-bg-1.jpg' },
+	{ 'id': '2', 'src': 'images/slider-bg-2.jpg' },
+	{ 'id': '3', 'src': 'images/slider-bg-3.jpg' },
 ];
 export default class Slider extends Component {
 	render() {
