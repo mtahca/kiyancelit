68105644427d476fd7c3cd7aa8efe72929f7bc5d
Mehmet Tahca <mtahca@Mehmets-iMac.local>
2019-10-25 17:34:51 +0300

    iletisim sayfası

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,8 @@ export default class Footer extends Component {
 					Kıyanç İnşaat © 2014<br></br>Adres: Orhaniye Mah.Hürriyet Cad.Kahyaoğlu Apt Kat :4 No:15 Muğla<br></br>Tel: 0 252 214 04 48 - 0 252 214 44 08</section>
 				<section className="eight columns">
 				</section>
-				<section class="eight columns">
-					<ul class="footer-nav">
+				<section className="eight columns">
+					<ul className="footer-nav">
 						<li>
 							<a href="http://www.teknofikir.com"
 							><img src="images/teknofikir.png" /></a>
